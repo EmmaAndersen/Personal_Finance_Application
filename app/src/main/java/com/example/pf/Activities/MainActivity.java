5a614ae47eaab051002e718fa3536d7c535fa72c
EmmaAndersen <48715807+EmmaAndersen@users.noreply.github.com>
2021-08-27 13:38:14 +0200

    Implemented Shared preferences to remember the first and lastname on the welcome dialog. If the user checks the checkbox for remembering the name and then closes the application. The next time the user opens it, the first and lastname will be remembered and the user do not need to enter it again.

--- app/src/main/java/com/example/pf/Activities/MainActivity.java
+++ app/src/main/java/com/example/pf/Activities/MainActivity.java
@@ -5,9 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ImageButton;
 
@@ -17,6 +20,13 @@ public class MainActivity extends AppCompatActivity {
 
     private ImageButton myBudgetButton, myIncomeButton;
     public static final String TABLE_NAME = "my_item_table";
+
+    private SharedPreferences preferences;
+    private SharedPreferences.Editor editor;
+
+    private EditText edt_firstName;
+    private EditText edt_lastName;
+    private CheckBox checkbox_remember;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,7 +53,7 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    private void showWelcomeDialog(){
+    private void showWelcomeDialog() {
         AlertDialog.Builder dialog = new AlertDialog.Builder(this);
         LayoutInflater inflater = LayoutInflater.from(this);
         View v = inflater.inflate(R.layout.welcome_layout, null);
@@ -53,16 +63,67 @@ public class MainActivity extends AppCompatActivity {
         myDialog.setCancelable(false);
 
         AppCompatButton btn_login = v.findViewById(R.id.btn_login);
-        EditText edt_firstName = v.findViewById(R.id.edt_firstname);
-        EditText edt_lastName = v.findViewById(R.id.edt_lastname);
+        checkbox_remember = v.findViewById(R.id.checkbox_remember);
+        edt_firstName = v.findViewById(R.id.edt_firstname);
+        edt_lastName = v.findViewById(R.id.edt_lastname);
+
+        preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        editor = preferences.edit();
+
+        sharedPreferencesCheck();
+
 
         btn_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //myDialog.dismiss();
+
+                if (checkbox_remember.isChecked()) {
+                    String firstName = edt_firstName.getText().toString();
+                    String lastName = edt_lastName.getText().toString();
+
+                    editor.putString(getString(R.string.firstName_key), firstName);
+                    editor.commit();
+
+                    editor.putString(getString(R.string.lastName_key), lastName);
+                    editor.commit();
+
+                    editor.putString(getString(R.string.checkbox_key), "True");
+                    editor.commit();
+                }
+                else {
+                    editor.putString(getString(R.string.firstName_key), "");
+                    editor.commit();
+
+                    editor.putString(getString(R.string.lastName_key), "");
+                    editor.commit();
+
+                    editor.putString(getString(R.string.checkbox_key), "False");
+                    editor.commit();
+                }
                 myDialog.dismiss();
             }
         });
 
         myDialog.show();
+    }
+
+    private void sharedPreferencesCheck() {
+
+        String mCheckbox = preferences.getString(getString(R.string.checkbox_key), "False");
+        String mFirstName = preferences.getString(getString(R.string.firstName_key), "");  //empty default value
+        String mLastName = preferences.getString(getString(R.string.lastName_key), "");  //empty default value
+
+        //Set the values to the TextViews.
+        edt_firstName.setText(mFirstName);
+        edt_lastName.setText(mLastName);
+
+        if (mCheckbox.equals("True")) {
+            checkbox_remember.setChecked(true);
+        } else {
+            checkbox_remember.setChecked(false);
+        }
+
+
     }
 }
